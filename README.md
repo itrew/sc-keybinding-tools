4dab104c0ca323252e1b87586baef0d33274dd3d
Ian Trewella <ian@itrew.net>
2024-03-29 17:41:27 -0700

    data extraction script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Star Citizen Control Utility
+
+## Extracting the latest data
+
+In order to extract the latest data from the game, the [`unp4k`](https://github.com/dolkensp/unp4k) utility is required.
+Create an `unp4k` folder at the root of this project, and copy the downloaded and extracted utility folder within it
+(e.g. `unp4k/unp4k-suite-v3.13.21/`). You can then extract the required files using `./bin/unpack_data.sh` or `npm run
+extract` (just a pointer to the shell script). This will delete and overwrite the previously extracted files into the
+`src/lib/data/extracted-files` directory for source control to track changes.
---
+++ bin/unpack_data.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+#
+# This script extracts the latest data files needed from the Data.p4k file.
+
+# Remove previous copies of extracted data.
+rm -rf "unp4k/Data/"
+rm -rf "src/lib/data/extracted-files"
+
+cd unp4k
+
+# Unpack the neccessary files from the Data.p4k file.
+./unp4k-suite-v3.13.21/unp4k.exe "/y/Roberts Space Industries/StarCitizen/LIVE/Data.p4k" defaultProfile.xml
+./unp4k-suite-v3.13.21/unp4k.exe "/y/Roberts Space Industries/StarCitizen/LIVE/Data.p4k" english/global.ini
+
+# Decode the XML files.
+./unp4k-suite-v3.13.21/unforge.exe "Data/Libs/Config/defaultProfile.xml"
+
+cd ..
+
+# Make a directory where the extracted files will be copied to for source control.
+mkdir -p "src/lib/data/extracted-files"
+
+# Copy the extracted and decoded files to a tracked folder.
+cp "unp4k/Data/Libs/Config/defaultProfile.xml" "src/lib/data/extracted-files/defaultProfile.xml"
+cp "unp4k/Data/Localization/english/global.ini" "src/lib/data/extracted-files/global.ini"
