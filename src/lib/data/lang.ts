419921525c07972918a6baacfe3e848c1e8b0d83
Ian Trewella <ian@itrew.net>
2024-04-06 00:22:21 -0700

    tooling and scripts to parse extracted data

---
+++ src/lib/data/lang.ts
@@ -0,0 +1,21 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+
+import { parse } from 'ini';
+
+// Read the global.ini file into a string.
+let fileContents = readFileSync(
+	path.resolve(import.meta.dirname, './extracted-files/global.ini'),
+	'utf-8',
+);
+
+// Remove some of the added ,P suffixes that shouldn't be included in the object keys
+fileContents = fileContents.replaceAll(',P=', '=');
+
+// Parse the language file into an object.
+export const lang = parse(fileContents) as { [key: string]: string };
+
+// Lookup a localized UI label or description.
+export const langLookupUI = (key: string | undefined): string | undefined => {
+	return key ? lang[key.slice(1)] || key : undefined;
+};
---
+++ src/lib/data/profile.ts
@@ -0,0 +1,43 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+
+import { XMLParser } from 'fast-xml-parser';
+
+export type ActionXML = {
+	$_name: string;
+	$_UILabel?: string;
+	$_UIDescription?: string;
+	$_optionGroup?: string;
+	$_keyboard?: string;
+	$_joystick?: string;
+	$_mouse?: string;
+	$_gamepad?: string;
+};
+
+export type ActionMapXML = {
+	$_name: string;
+	$_version: string;
+	$_UILabel: string;
+	$_UICategory: string;
+	action: ActionXML[];
+};
+
+export type ProfileXML = {
+	profile: {
+		actionmap: ActionMapXML[];
+	};
+};
+
+// Configure the parser.
+const parser = new XMLParser({
+	ignoreAttributes: false,
+	attributeNamePrefix: '$_',
+});
+
+// Read the xml contents of the default profile into a buffer.
+const fileContents = readFileSync(
+	path.resolve(import.meta.dirname, './extracted-files/defaultProfile.xml'),
+);
+
+// Parse the xml profile file into an object.
+export const defaultProfile: ProfileXML = parser.parse(fileContents);
