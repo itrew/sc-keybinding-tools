6752645d8e11e814a0e112e71419bc094bc00284
Ian Trewella <ian@itrew.net>
2024-04-12 01:37:23 -0700

    exclude keyboard axis for the time being

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,13 @@
-import actionData from '$lib/data/processed-files/actionData.json';
+import actionData from '$lib/data/processed-files/action-map-data.json';
+
+const data: ActionMap[] = actionData;
+
+// Disable keyboard axis binding for display.
+data.forEach((am) => {
+	am.actions.forEach((a) => {
+		a.keyboard.axis = false;
+	})
+});
 
 export const load = async () => {
 	return {
