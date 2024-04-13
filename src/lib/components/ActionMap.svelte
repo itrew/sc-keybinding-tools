305d7976bc2093a69b7c48e2869549db19824601
Ian Trewella <ian@itrew.net>
2024-04-13 02:38:34 -0700

    add meltui and first collapsible

--- src/lib/components/ActionMap.svelte
+++ src/lib/components/ActionMap.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { createCollapsible, melt } from '@melt-ui/svelte';
+	import { slide } from 'svelte/transition';
 	import ActionRow from './ActionRow.svelte';
 	import { isActionBindable } from '$lib/util';
 
@@ -11,6 +13,11 @@
 	let nonBindableActions: Action[] = actionMap.actions.filter((a) => !isActionBindable(a));
 
 	let bindable: boolean = bindableActions.length > 0;
+
+	const {
+		elements: { root, content, trigger },
+		states: { open },
+	} = createCollapsible();
 </script>
 
 <div
@@ -27,7 +34,7 @@
 		{/if}
 		<div class="justify-self-end text-base-subtle">({actionCount} actions)</div>
 	</div>
-	<div class="grid grid-cols-6 pb-0.5">
+	<div use:melt={$root} class="grid grid-cols-6 pb-0.5">
 		<div class="mb-0.5 bg-surface-3 p-2 text-xs text-base-subtle">Action</div>
 		<div class="mb-0.5 bg-surface-3 p-2 text-xs text-base-subtle">In-Game Label</div>
 		<div class="mb-0.5 bg-surface-3 p-2 text-xs text-base-subtle">Mouse</div>
@@ -39,9 +46,16 @@
 				<ActionRow {action} bindable={true} />
 			{/each}
 			{#if nonBindableActions.length > 0}
-				{#each nonBindableActions as action}
-					<ActionRow {action} bindable={false} />
-				{/each}
+				<div use:melt={$trigger} class="col-span-6 p-2 text-xs text-error opacity-70">
+					Actions not mappable in game ({nonBindableActions.length})
+				</div>
+				{#if $open}
+					<div use:melt={$content} transition:slide class="col-span-6 grid grid-cols-subgrid">
+						{#each nonBindableActions as action}
+							<ActionRow {action} bindable={false} />
+						{/each}
+					</div>
+				{/if}
 			{/if}
 		{:else}
 			{#each nonBindableActions as action}
