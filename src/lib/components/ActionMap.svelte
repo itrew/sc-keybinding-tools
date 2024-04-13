5a801512daaa13812ce5e4273a6b683865918741
Ian Trewella <ian@itrew.net>
2024-04-12 23:07:36 -0700

    sort non-bindable actions to bottom of map

--- src/lib/components/ActionMap.svelte
+++ src/lib/components/ActionMap.svelte
@@ -6,22 +6,23 @@
 
 	let label: string = actionMap.attributes.labelLocal || actionMap.name;
 	let actionCount: number = actionMap.actions.length;
+	let actions = actionMap.actions.sort((a, b) => +isActionBindable(b) - +isActionBindable(a));
 </script>
 
 <div
-	class="mb-4 overflow-hidden rounded-lg border border-solid border-base border-opacity-10 bg-surface-1"
+	class="mb-4 bg-surface-1 overflow-hidden border border-base border-solid border-opacity-10 rounded-lg"
 >
-	<div class="flex justify-between bg-surface-2 p-2 align-bottom">
+	<div class="p-2 bg-surface-2 flex justify-between items-center">
 		<div class="text-lg font-semibold text-base-emphasized" title={actionMap.name}>{label}</div>
-		<div class="text-base-subtle">({actionCount})</div>
+		<div class="text-base-subtle">({actionCount} actions)</div>
 	</div>
 	<div class="grid grid-cols-5">
 		<div class="bg-surface-3 p-2 text-xs text-base-subtle">Action</div>
-		<div class="bg-surface-3 p-2 text-xs text-base-subtle">Label</div>
+		<div class="bg-surface-3 p-2 text-xs text-base-subtle">In-Game Label</div>
 		<div class="bg-surface-3 p-2 text-xs text-base-subtle">Mouse</div>
 		<div class="bg-surface-3 p-2 text-xs text-base-subtle">Keyboard</div>
 		<div class="bg-surface-3 p-2 text-xs text-base-subtle">Joystick</div>
-		{#each actionMap.actions as action}
+		{#each actions as action}
 			<ActionRow {action} bindable={isActionBindable(action)} />
 		{/each}
 	</div>
