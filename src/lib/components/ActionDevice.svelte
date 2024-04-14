19a0db571bf600ac9e4368cb68e082be69973c74
Ian Trewella <ian@itrew.net>
2024-04-13 22:50:09 -0700

    better flex use in action device

--- src/lib/components/ActionDevice.svelte
+++ src/lib/components/ActionDevice.svelte
@@ -8,25 +8,22 @@
 
 	let bindable = isDeviceBindable({ button, axis });
 	let backgroundColor = bindable ? 'bg-surface-2' : '';
+	let opacity = bindable ? 1 : 0.45;
 </script>
 
-<div class="px-2 py-1 flex items-center justify-between rounded-sm border border-solid border-base border-opacity-5 {backgroundColor} ">
-	{#if button !== false || axis !== false}
-		<span class="flex gap-x-1 text-primary {!bindable ? 'opacity-60' : ''}">
-			{#if button}
-				<Icon icon="button" />
-			{:else if button === null}
-				<Icon icon="unknown-button" />
-			{/if}
-			{#if axis}
-				<Icon icon="axis" />
-			{:else if axis === null}
-				<Icon icon="unknown-axis" />
-			{/if}
-		</span>
+<div class="px-2 py-1 flex gap-x-1 items-center rounded-sm border border-solid border-base border-opacity-5 {backgroundColor} text-primary">
+	{#if button}
+		<Icon icon="button" {opacity}/>
+	{:else if button === null}
+		<Icon icon="unknown-button" {opacity}/>
+	{/if}
+	{#if axis}
+		<Icon icon="axis" {opacity}/>
+	{:else if axis === null}
+		<Icon icon="unknown-axis" {opacity}/>
 	{/if}
 	{#if defaultBinding}
-		<div class="font-mono text-base-subtle {!bindable ? 'text-opacity-50' : ''}">
+		<div class="grow text-right font-mono text-base-subtle { bindable ? '' : 'opacity-45'}">
 			{defaultBinding}
 		</div>
 	{/if}
