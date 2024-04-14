f0f44edff54a8211da3ce15832cc9bbbdbd2c2ee
Ian Trewella <ian@itrew.net>
2024-04-13 18:31:17 -0700

    subgrid and use gap

--- src/lib/components/ActionDevice.svelte
+++ src/lib/components/ActionDevice.svelte
@@ -5,14 +5,12 @@
 	export let button: boolean | null;
 	export let axis: boolean | null;
 	export let defaultBinding: string = '';
-	let classNames: string = '';
-	export { classNames as class };
 
 	let bindable = isDeviceBindable({ button, axis });
 	let backgroundColor = bindable ? 'bg-surface-2' : '';
 </script>
 
-<div class="{classNames} {backgroundColor} ">
+<div class="px-2 py-1 flex items-center justify-between rounded-sm border border-solid border-base border-opacity-5 {backgroundColor} ">
 	{#if button !== false && axis !== false}
 		<span class="flex gap-x-1 text-primary {!bindable ? 'opacity-60' : ''}">
 			{#if button}
--- src/lib/components/ActionMap.svelte
+++ src/lib/components/ActionMap.svelte
@@ -34,45 +34,47 @@
 		{/if}
 		<div class="justify-self-end text-base-subtle">({actionCount} actions)</div>
 	</div>
-	<div use:melt={$root} class="grid grid-cols-6 pb-0.5">
-		<div class="mb-0.5 bg-surface-3 p-2 text-xs text-base-subtle">Action</div>
-		<div class="mb-0.5 bg-surface-3 p-2 text-xs text-base-subtle">In-Game Label</div>
-		<div class="mb-0.5 bg-surface-3 p-2 text-xs text-base-subtle">Mouse</div>
-		<div class="mb-0.5 bg-surface-3 p-2 text-xs text-base-subtle">Keyboard</div>
-		<div class="mb-0.5 bg-surface-3 p-2 text-xs text-base-subtle">Gamepad</div>
-		<div class="mb-0.5 bg-surface-3 p-2 text-xs text-base-subtle">Joystick</div>
-		{#if bindable}
-			{#each bindableActions as action}
-				<ActionRow {action} bindable={true} />
-			{/each}
-			{#if nonBindableActions.length > 0}
-				<div
-					use:melt={$trigger}
-					class="col-span-6 flex cursor-pointer items-center gap-x-2 p-2 text-sm text-error opacity-90"
-				>
-					<span>
-						Actions not mappable in game ({nonBindableActions.length})
-					</span>
-					<span>
-						{#if $open}
-							<Icon icon="minus-square" size={22}/>
-						{:else}
-							<Icon icon="plus-square" size={22}/>
-						{/if}
-					</span>
-				</div>
-				{#if $open}
-					<div use:melt={$content} class="col-span-6 grid grid-cols-subgrid">
-						{#each nonBindableActions as action}
-							<ActionRow {action} bindable={false} />
-						{/each}
+	<div use:melt={$root} class="grid grid-cols-6">
+		<div class="p-2 pl-3 bg-surface-3 text-xs text-base-subtle">Action</div>
+		<div class="p-2 bg-surface-3 text-xs text-base-subtle">In-Game Label</div>
+		<div class="p-2 bg-surface-3 text-xs text-base-subtle">Mouse</div>
+		<div class="p-2 bg-surface-3 text-xs text-base-subtle">Keyboard</div>
+		<div class="p-2 bg-surface-3 text-xs text-base-subtle">Gamepad</div>
+		<div class="p-2 bg-surface-3 text-xs text-base-subtle">Joystick</div>
+		<div class="p-1 col-span-6 grid grid-cols-subgrid gap-1">
+			{#if bindable}
+				{#each bindableActions as action}
+					<ActionRow {action} bindable={true} />
+				{/each}
+				{#if nonBindableActions.length > 0}
+					<div
+						use:melt={$trigger}
+						class="col-span-6 flex cursor-pointer items-center gap-x-2 p-2 text-sm text-error opacity-90"
+					>
+						<span>
+							Actions not mappable in game ({nonBindableActions.length})
+						</span>
+						<span>
+							{#if $open}
+								<Icon icon="minus-square" size={22}/>
+							{:else}
+								<Icon icon="plus-square" size={22}/>
+							{/if}
+						</span>
 					</div>
+					{#if $open}
+						<div use:melt={$content} class="col-span-6 grid grid-cols-subgrid">
+							{#each nonBindableActions as action}
+								<ActionRow {action} bindable={false} />
+							{/each}
+						</div>
+					{/if}
 				{/if}
+			{:else}
+				{#each nonBindableActions as action}
+					<ActionRow {action} bindable={false} />
+				{/each}
 			{/if}
-		{:else}
-			{#each nonBindableActions as action}
-				<ActionRow {action} bindable={false} />
-			{/each}
-		{/if}
+		</div>
 	</div>
 </div>
