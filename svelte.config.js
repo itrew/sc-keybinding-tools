9b116f09803f5f6ce945c1ada1a3749c4bb97b73
Ian Trewella <ian@itrew.net>
2024-04-13 23:17:12 -0700

    configure netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
